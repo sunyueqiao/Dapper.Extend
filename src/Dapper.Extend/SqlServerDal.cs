03025952f509400ff2b166643141e57f46009c9e
sunyueqiao <sunyueqiao@jd.com>
2019-03-07 09:56:24 +0800

    抽象类中的方法改为虚方法

--- src/Dapper.Extend/SqlServerDal.cs
+++ src/Dapper.Extend/SqlServerDal.cs
@@ -19,37 +19,37 @@ namespace Dapper.Extend
             this._baseSqlBuilder = SqlServerSqlBuilder<TEntity>.Build();
         }
 
-        public TPrimary Insert(TEntity entity)
+        public virtual TPrimary Insert(TEntity entity)
         {
             SqlObjectData sqlObject = this._baseSqlBuilder.BuildInsert(entity);
             return this._dapperExtension.Insert<TPrimary>(sqlObject.Sql, sqlObject.Parameters);
         }
 
-        public int Update(TEntity entity)
+        public virtual int Update(TEntity entity)
         {
             SqlObjectData sqlObject = this._baseSqlBuilder.BuildUpdate(entity);
             return this._dapperExtension.Update(sqlObject.Sql, sqlObject.Parameters);
         }
 
-        public int Delete(TEntity entity)
+        public virtual int Delete(TEntity entity)
         {
             SqlObjectData sqlObject = this._baseSqlBuilder.BuildDelete(entity);
             return this._dapperExtension.Delete(sqlObject.Sql, sqlObject.Parameters);
         }
 
-        public IEnumerable<TEntity> Select(TEntity entity)
+        public virtual IEnumerable<TEntity> Select(TEntity entity)
         {
             SqlObjectData sqlObjectData = this._baseSqlBuilder.BuildSelect(entity);
             return this._dapperExtension.Select<TEntity>(string.Empty, null);
         }
 
-        public TEntity SelectEntity(TEntity entity)
+        public virtual TEntity SelectEntity(TEntity entity)
         {
             SqlObjectData sqlObjectData = this._baseSqlBuilder.BuildSelect(entity);
             return this._dapperExtension.SelectOne<TEntity>(sqlObjectData.Sql, sqlObjectData.Parameters);
         }
 
-        public IEnumerable<TEntity> Select(string sql, DynamicParameters parameters)
+        public virtual IEnumerable<TEntity> Select(string sql, DynamicParameters parameters)
         {
             return this._dapperExtension.Select<TEntity>(sql, parameters);
         }
